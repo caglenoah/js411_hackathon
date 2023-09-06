90e63bcfca469818f872daa382e403c78749445b
austinc512 <cello.austin1994@gmail.com>
2023-09-06 01:53:17 -0500

    SearchForm and App components working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchForm from "./components/SearchForm";
 
 function App() {
+  const [data, setData] = useState([]);
+  const popularSearch = "http://hn.algolia.com/api/v1/search?";
+  const [params, setParams] = useState([]);
+  // additional state variables can be declared here
+
+  useEffect(() => {
+    // fetch data based on SearchForm component
+    console.log(`useEffect triggered`);
+    console.log(`params: `, params);
+    let fullUrl = popularSearch + params.join("&");
+    if (params && params.length > 0) {
+      fullUrl += "&hitsPerPage=35";
+    } else {
+      fullUrl += "hitsPerPage=35";
+    }
+    console.log("full Url: ", fullUrl);
+    fetch(fullUrl)
+      .then((response) => response.json())
+      .then((data) => setData(data.hits));
+    // END fetch data
+  }, [params]);
+
+  // the data variable will contain the posts that need to be used
+  // other child components can consume the data array and render data in a consistent format.
+  // the above useEffect hook is responsible for fetching the data based on the SearchForm component
+
+  // other components should use the data variable (and use [data] in the dependency array if using a useEffect hook)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchForm params={params} setParams={setParams} />
+      <h1>Results</h1>
+      <ul>
+        {data.map((results) => {
+          return <li key={results.created_at}>{results.title}</li>;
+        })}
+      </ul>
+      {/* this <ul> element should be replaced by the ListArticles component*/}
     </div>
   );
 }
---
+++ src/components/SearchForm.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+const SearchForm = (props) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [timeInput, setTimeInput] = useState("");
+
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const createTimeInput = (event) => {
+    const newTime = event.target.value;
+    setTimeInput(newTime);
+  };
+
+  const createTimeOutput = (str) => {
+    // time values are in seconds because that's what the API wants
+    const now = Math.trunc(Date.now() / 1000);
+    if (str === "All time") {
+      return "";
+    } else if (str === "Last 24hr") {
+      return `numericFilters=created_at_i>${now - 24 * 60 * 60}`;
+    } else if (str === "Past week") {
+      return `numericFilters=created_at_i>${now - 24 * 60 * 60 * 7}`;
+    } else if (str === "Past month") {
+      return `numericFilters=created_at_i>${now - 24 * 60 * 60 * 31}`;
+    } else if (str === "Past year") {
+      return `numericFilters=created_at_i>${now - 24 * 60 * 60 * 365}`;
+    } else {
+      return "";
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTimeOutput = createTimeOutput(timeInput);
+    // props.setParams([newTimeOutput]);
+    if (searchQuery !== "") {
+      // if no searchQuery exists
+      const newQuery = `query=${searchQuery}`;
+      props.setParams([newTimeOutput, newQuery]);
+    } else {
+      props.setParams([newTimeOutput]);
+    }
+    // I think this is the only set of query params that need to be supported
+  };
+
+  return (
+    <div>
+      <form className="search" onSubmit={handleSubmit}>
+        <p className="hn-logo">H</p>
+        <label>Search Hacker News </label>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
+      <p className="in-range">In range </p>
+      <select value={timeInput} onChange={createTimeInput}>
+        <option value="All time">All time</option>
+        <option value="Last 24hr">Last 24hr</option>
+        <option value="Past week">Past week</option>
+        <option value="Past month">Past month</option>
+        <option value="Past year">Past year</option>
+      </select>
+    </div>
+  );
+};
+
+export default SearchForm;
