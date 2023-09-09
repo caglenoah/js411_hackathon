1001c9d7ed4c5d33d3cc3ed93e6b5325e57105fa
ddiza <dantediza@pm.me>
2023-09-09 16:05:31 -0500

    updated footer file

---
+++ src/Footer.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <>
+    <div>
+    <tr>
+    <td>
+    <img src="s.gif" height="10" width="0" alt="line" />
+            <table width="100%" cellspacing="0" cellpadding="1">
+                <tbody>
+                    <tr>
+                    <td bgcolor="#ff6600"></td></tr>
+                </tbody></table>
+           </td>
+           </tr>
+    </div>
+    <br/>  
+      
+    
+    <div>
+    <center>
+        <span class="yclinks">
+            <a href="newsguidelines.html">Guidelines</a>
+            <a> |  </a>
+            <a href="newsfaq.html">FAQ</a>
+            <a> |  </a>
+            <a href="lists">Lists</a>
+            <a> |  </a>
+            <a href="https://github.com/HackerNews/API">API</a>
+            <a> |  </a>
+            <a href="security.html">Security</a>
+            <a> |  </a>
+            <a href="https://www.ycombinator.com/legal/">Legal</a>
+            <a> |  </a>
+            <a href="https://www.ycombinator.com/apply/">Apply to YC</a>
+            <a> |  </a>
+            <a href="mailto:hn@ycombinator.com">Contact</a>
+        </span>
+    <br />
+    </center>
+    </div>;
+    </>
+
+    );
+};
+
+export default Footer;
+
+
+
+
+
+
+// FROM actual HN site:
+/* <tr>
+    <td>
+        <img src="s.gif" height="10" width="0">
+            <table width="100%" cellspacing="0" cellpadding="1">
+                <tbody><tr>
+                    <td bgcolor="#ff6600"></td>
+           </tbody></table>
+           </td>
+           </tr>
+<br/>
+
+<center>
+    <span class="yclinks"
+    ><a href="newsguidelines.html">Guidelines</a> | 
+    <a href="newsfaq.html">FAQ</a> | 
+    <a href="lists">Lists</a> | 
+    <a href="https://github.com/HackerNews/API">API</a> | 
+    <a href="security.html">Security</a> | 
+    <a href="https://www.ycombinator.com/legal/">Legal</a> | 
+    <a href="https://www.ycombinator.com/apply/">Apply to YC</a> | 
+    <a href="mailto:hn@ycombinator.com">Contact</a>
+    </span>
+    <br />
+</center> */
