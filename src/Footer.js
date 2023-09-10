06d9fe9d970a2705496a439625dc1d72c2ffcfe1
austinc512 <cello.austin1994@gmail.com>
2023-09-10 13:22:15 -0500

    need to pull from main again

--- src/Footer.js
+++ src/Footer.js
@@ -1,54 +1,50 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
     <>
-    <div>
-    <tr>
-    <td>
-        <img src="https://news.ycombinator.com/s.gif" alt="orange line" />
+      <div>
+        <tr>
+          <td>
+            <img src="https://news.ycombinator.com/s.gif" alt="orange line" />
             <table width="100%" cellspacing="0" cellpadding="1">
-                <tbody>
-                    <tr>
-                    <td bgcolor="#ff6600"></td></tr>
-                </tbody></table>
-           </td>
-           </tr>
-    </div>
-    <br/>  
-      
-    
-    <div>
-    <center>
-        <span class="yclinks">
-            <a href="https://news.ycombinator.com/newsguidelines.html">Guidelines</a>
-            <a> |  </a>
+              <tbody>
+                <tr>
+                  <td bgcolor="#ff6600"></td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </div>
+      <br />
+      <div>
+        <center>
+          <span class="yclinks">
+            <a href="https://news.ycombinator.com/newsguidelines.html">
+              Guidelines
+            </a>
+            <a> | </a>
             <a href="https://news.ycombinator.com/newsfaq.html">FAQ</a>
-            <a> |  </a>
+            <a> | </a>
             <a href="https://news.ycombinator.com/lists">Lists</a>
-            <a> |  </a>
+            <a> | </a>
             <a href="https://github.com/HackerNews/API">API</a>
-            <a> |  </a>
+            <a> | </a>
             <a href="https://news.ycombinator.com/security.html">Security</a>
-            <a> |  </a>
+            <a> | </a>
             <a href="https://www.ycombinator.com/legal/">Legal</a>
-            <a> |  </a>
+            <a> | </a>
             <a href="https://www.ycombinator.com/apply/">Apply to YC</a>
-            <a> |  </a>
+            <a> | </a>
             <a href="mailto:hn@ycombinator.com">Contact</a>
-        </span>
-    <br />
-    </center>
-    </div>;
+          </span>
+          <br />
+        </center>
+      </div>
+      ;
     </>
-
-    );
-};
+  );
+}
 
 export default Footer;
-
-
-
-
-
-
